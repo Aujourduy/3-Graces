377037f7b027b86bd25f37f772322fe0b7e85ab8
Duy Dang <aujourduy@gmail.com>
2026-03-27 09:04:51 +0100

    refactor: Restauration design original pré-Figma + adaptation logo Stop & Dance
    - Restauré 6 fichiers de vues au design original (commit 476b93b)
      * _hero.html.erb : section hero simplifiée
      * _date_separator.html.erb : séparateurs gris horizontaux
      * _event_card.html.erb : images 80x80, tags classiques
      * _filters.html.erb : fond blanc avec header terracotta
      * _newsletter_signup.html.erb : style original
      * index.html.erb : layout original
    
    - Adapté logo de "3 Grâces" → "Stop & Dance" dans hero
    
    - Corrigé tests pour correspondre au design restauré
      * pages_accessibility_test.rb : "Liste des événements" → "Agenda des Ateliers"
      * events_test.rb : idem
    
    Tests : 71 unitaires + 8 système, 0 failures

--- test/integration/pages_accessibility_test.rb
+++ test/integration/pages_accessibility_test.rb
@@ -55,7 +55,7 @@ class PagesAccessibilityTest < ActionDispatch::IntegrationTest
   test "events index is accessible" do
     get evenements_path, headers: @headers
     assert_response :success
-    assert_select "h1", text: /Liste des événements/
+    assert_select "h1", text: /Agenda des Ateliers/
   end
 
   test "event show page is accessible" do
